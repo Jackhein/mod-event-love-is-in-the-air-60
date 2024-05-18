f46edec1277d09f968ca29bd1aae376b3ea4a4bf
jackhein <moustiquejunior@gmail.com>
2024-05-18 02:06:26 +0200

    Add DBC data through SQL request

---
+++ data/sql/db-world/event_love_is_in_the_air_60_item_dbc.sql
@@ -0,0 +1,3 @@
+-- Fumigator's Mask
+INSERT IGNORE INTO item_dbc (`ID`, `ClassID`, `SubclassID`, `Sound_Override_Subclassid`, `Material`, `DisplayInfoID`, `InventoryType`, `SheatheType`)
+ VALUES (60000, 4, 1, -1, 4, 64043, 1, 0);
